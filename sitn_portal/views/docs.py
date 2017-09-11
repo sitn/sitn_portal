a0a282391a10eeea4d5c43fbd910eefca3a4f776
Francois Voisard <francois.voisard@ne.ch>
2017-09-11 16:01:56 +0200

    flaked project

--- sitn_portal/views/docs.py
+++ sitn_portal/views/docs.py
@@ -4,15 +4,18 @@ from pyramid.view import view_config
 from sitn_portal.models import DBSession
 from sitn_portal.models import Documents
 
+
 @view_config(route_name='docs', renderer='docs.html')
 def docs(request):
 
     return {'one': 'one', 'project': 'sitn_portal'}
 
+
 @view_config(route_name='get', renderer='docs.html')
 def get(request, docid):
 
     return {'document': 'docid'}
+
 
 @view_config(route_name='list', renderer='json')
 def list(request, docid=None):
@@ -51,72 +54,5 @@ def list(request, docid=None):
             if document.revisiondate else None,
             'operator': document.operator
         })
-    docs = [{
-        'docid': 'NE0000D199110020001fr',
-        'legalstate': 1,
-        'abbreviation': 'LCAT',
-        'sanctiondate': '',
-        'operator': 'acn\\voisardf',
-        'localurl': '',
-        'chmunicipalitynb': 0,
-        'municipalitynb': 0,
-        'title': '',
-        'state': 'NE',
-        'publicationdate': '1991-10-02',
-        'officialtitle': 'Loi cantonale sur l\'aménagement du territoire',
-        'officialnb': 'RSN 701.0',
-        'remoteurl': 'http://rsn.ne.ch/ajour/dati/f/pdf/7010.pdf',
-        'revisiondate': '',
-        'lang': 'fr',
-        'abolishingdate': '',
-        'doctype': 1,
-        'cadastrenb': 0,
-        'municipalityname': '',
-        'entrydate': ''
-        }, {
-        'docid': 'BL0000D199110020002de',
-        'legalstate': 1,
-        'abbreviation': 'LCAT',
-        'sanctiondate': '',
-        'operator': 'acn\\voisardf',
-        'localurl': '',
-        'chmunicipalitynb': 0,
-        'municipalitynb': 0,
-        'title': '',
-        'state': 'BL',
-        'publicationdate': '1991-10-02',
-        'officialtitle': 'Loi cantonale sur l\'aménagement du territoire',
-        'officialnb': 'RSN 701.0',
-        'remoteurl': 'http://rsn.ne.ch/ajour/dati/f/pdf/7010.pdf',
-        'revisiondate': '',
-        'lang': 'de',
-        'abolishingdate': '',
-        'doctype': 1,
-        'cadastrenb': 0,
-        'municipalityname': '',
-        'entrydate': ''
-        }, {
-        'docid': 'NE0000D199110020003fr',
-        'legalstate': 1,
-        'abbreviation': 'LCAT',
-        'sanctiondate': '1991-10-02',
-        'operator': 'acn\\voisardf',
-        'localurl': '',
-        'chmunicipalitynb': 0,
-        'municipalitynb': 0,
-        'title': '',
-        'state': 'NE',
-        'publicationdate': '',
-        'officialtitle': 'Loi cantonale sur l\'aménagement du territoire',
-        'officialnb': 'RSN 701.0',
-        'remoteurl': 'http://rsn.ne.ch/ajour/dati/f/pdf/7010.pdf',
-        'revisiondate': '',
-        'lang': 'fr',
-        'abolishingdate': '',
-        'doctype': 1,
-        'cadastrenb': 0,
-        'municipalityname': '',
-        'entrydate': '1991-10-02',
-        }]
 
     return {'documents': doclist}
