9ca6c02c397d001e70e86a3f41a1903c0d6e3bb3
Francois Voisard <francois.voisard@ne.ch>
2018-02-23 16:06:34 +0100

    added new routes for adding, editing and deleting docs

--- sitn_portal/routes.py
+++ sitn_portal/routes.py
@@ -6,4 +6,8 @@ def includeme(config):
 
     # docs.py
     config.add_route('list', 'docs/list')
+    config.add_route('search', 'docs/search/{term}')
     config.add_route('get', 'docs/get/{docid}')
+    config.add_route('add', 'docs/add')
+    config.add_route('edit', 'docs/edit/{docid}')
+    config.add_route('delete', 'docs/delete')
